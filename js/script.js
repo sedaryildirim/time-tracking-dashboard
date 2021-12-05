553881b109a1c2568045f5e07d73baa231a5b455
Sedar Yildirim <40918409+sedaryildirim@users.noreply.github.com>
2021-12-05 10:00:01 +0000

    index and js updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
-fetch ('data.json')
-    .then(results => results.json())
-    .then((data) => {
-       
-        const dailyCurrent = data[0].timeframes.daily.current;
-        const dailyPrevious = data[0].timeframes.daily.previous;
+// line 2 selects all items with a class of .list-item
+const items = document.querySelectorAll('.list-item');
 
-        document.querySelector('.card__daily-hours').innerHTML = dailyCurrent + 'hrs';
-        document.querySelector('.card__previous-hours').innerHTML = dailyPrevious + 'hrs';
+// line 5-6 adds event listener of onClick to all .list-items 
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        
+        // line 8-9 add .active class to clicked on .list-item while removing .active from others
+        items.forEach(e => e.classList.remove('active'));
+        item.classList.add('active');
+        getData(item.innerHTML.toLowerCase());
+    })
+});
 
-        console.log(dailyCurrent);
-        console.log(dailyPrevious);
-    });
+// line 16-17 get data from our data.json file while referencing jQuery
+const getData = (dateType) => {
+    const data = $.getJSON("data.json", (data) => {
+        data.forEach((item) => {
+            // lines 20-22 assign variables to item.title, and current and previous timeframes
+            const title = item.title.toLowerCase();
+            const current = item.timeframes[dateType].current;
+            const previous = item.timeframes[dateType].previous;
+            
+            handleInnerHTML(title, current, previous);
+        });
+    })
+}
+
+const handleInnerHTML = (title, current, previous) => {
+    if (title === 'self care') {
+        const currentHours = document.querySelector(`.self-care-hours`);
+        const prevHours = document.querySelector(`.self-care-previous`);
+        currentHours.innerHTML = `${current}hrs`;
+        prevHours.innerHTML = `Previous - ${previous}hrs`;
+    } else {
+        const currentHours = document.querySelector(`.${title}-hours`);
+        const prevHours = document.querySelector(`.${title}-previous`);
+        currentHours.innerHTML = `${current}hrs`;
+        prevHours.innerHTML = `Previous - ${previous}hrs`;
+    }
+}
